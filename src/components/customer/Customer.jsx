5a8520aaf7733d604224e8c9305ff8598b933312
NgocPhuc89 <ngocphuc966@gmail.com>
2023-10-18 10:26:49 +0700

    up

--- src/components/customer/Customer.jsx
+++ src/components/customer/Customer.jsx
@@ -15,6 +15,7 @@ const Customer = () => {
             address: "phan chu trinh"
         }
     ]);
+    const [customer, setCustomer] = useState({});
     const city = [
         { id: 1, name: "Hà Nội" },
         { id: 2, name: "TP HCM" },
@@ -30,24 +31,26 @@ const Customer = () => {
         const list = customerList.filter((item) => item.id != id)
         setCustomerList(list)
     }
+
     const handleInput = (e) => {
-        console.log(e.target.value);
-        const customer = {
+        setCustomer(prev => ({
+            ...prev,
             id: uuidv4().slice(0, 8),
-            [e.target.name]: e.target.value
-        }
+            [e.target.name]: e.target.value,
+        }))
+    }
+    const handleCreate = () => {
         setCustomerList([
             ...customerList,
             customer
         ])
-        document.getElementById('exampleModal').click();
     }
 
     return (
         <div>
             <h1 className="text-center text-danger">Customer List</h1>
             <div data-bs-toggle="modal"
-                data-bs-target="#exampleModal">
+                data-bs-target="#createModal">
                 <button className="btn btn-primary">Create</button>
             </div>
             <table className="table table-striped">
@@ -86,111 +89,111 @@ const Customer = () => {
 
                 </tbody>
             </table>
-            <form>
-                <div
-                    className="modal fade"
-                    id="exampleModal"
-                    tabIndex={-1}
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                >
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">
-                                    Modal title
-                                </h5>
+            <div
+                className="modal fade"
+                id="createModal"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabIndex={-1}
+                aria-labelledby="staticBackdropLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel" >
+                                Create Customer
+                            </h5>
+                        </div>
+                        <div className="row ms-3 m-2 ">
+                            <div className="col-lg-6">
+                                <label htmlFor="">FullName</label>
+                                <input type="text" name="fullname" id="fullname"
+                                    onChange={handleInput}
+                                />
                             </div>
-                            <div className="row ms-3 m-2 ">
-                                <div className="col-lg-6">
-                                    <label htmlFor="">FullName</label>
-                                    <input type="text" name="fullname" id="fullname"
-                                        onChange={handleInput}
-                                    />
-                                </div>
-                                <div className="col-lg-6">
-                                    <label htmlFor="">Email</label>
-                                    <input type="email" name="email" id="email"
-                                        onChange={handleInput}
-                                    />
-                                </div>
+                            <div className="col-lg-6">
+                                <label htmlFor="">Email</label>
+                                <input type="email" name="email" id="email"
+                                    onChange={handleInput}
+                                />
                             </div>
-                            <div className="row ms-3 m-2">
-                                <div className="col-lg-6">
-                                    <label htmlFor="">Phone</label>
-                                    <input type="number" name="phone" id="phone"
-                                        onChange={handleInput}
-                                    />
-                                </div>
-                                <div className="col-lg-6">
-                                    <label htmlFor="">Address</label>
-                                    <input type="text" name="address" id="address"
-                                        onChange={handleInput}
-                                    />
-                                </div>
+                        </div>
+                        <div className="row ms-3 m-2">
+                            <div className="col-lg-6">
+                                <label htmlFor="">Phone</label>
+                                <input type="text" name="phone" id="phone"
+                                    onChange={handleInput}
+                                />
                             </div>
-                            <div className="row ms-3 ">
-                                <div className="col-lg-6">
-                                    <label htmlFor="">City</label>
-                                    <select
-                                        className="form-select"
-                                        multiple=""
-                                        aria-label="multiple select example"
-                                        id="city"
-                                        onChange={handleInput}
-                                    >
-                                        <option defaultValue="">-Vui Lòng Chọn-</option>
-                                        {
-                                            city.map(item => (
-                                                <option value={item.name} key={item.id}>{item.name}</option>
-                                            ))
-                                        }
-                                    </select>
-                                </div>
-                                <div className="col-lg-6 mb-5">
-                                    <label htmlFor="" className="" onChange={handleInput}>Gender
-                                        <div className="d-flex">
-                                            <div className="form-check me-3">
-                                                <input
-                                                    className="form-check-input"
-                                                    type="radio"
-                                                    name="flexRadioDefault"
-                                                    id="flexRadioDefault1"
-                                                    value="male"
-                                                />
-                                                <label className="form-check-label" htmlFor="flexRadioDefault1">Male</label>
-                                            </div>
-                                            <div className="form-check">
-                                                <input
-                                                    className="form-check-input"
-                                                    type="radio"
-                                                    name="flexRadioDefault"
-                                                    id="flexRadioDefault2"
-                                                    defaultChecked=""
-                                                    value="female"
-                                                />
-                                                <label className="form-check-label" htmlFor="flexRadioDefault2"> Female</label>
-                                            </div>
-                                        </div>
-                                    </label>
-                                </div>
+                            <div className="col-lg-6">
+                                <label htmlFor="">Address</label>
+                                <input type="text" name="address" id="address"
+                                    onChange={handleInput}
+                                />
                             </div>
-
-                            <div className="modal-footer">
-                                <button
-                                    type="button"
-                                    className="btn btn-secondary"
-                                    data-bs-dismiss="modal"
+                        </div>
+                        <div className="row ms-3 ">
+                            <div className="col-lg-6">
+                                <label htmlFor="">City</label>
+                                <select
+                                    className="form-select"
+                                    multiple=""
+                                    aria-label="multiple select example"
+                                    id="city"
+                                    name="city"
+                                    onChange={handleInput}
                                 >
-                                    Close
-                                </button>
-                                <button className="btn btn-primary">Save changes</button>
+                                    <option defaultValue="">-Vui Lòng Chọn-</option>
+                                    {
+                                        city.map(item => (
+                                            <option value={item.name} key={item.id}>{item.name}</option>
+                                        ))
+                                    }
+                                </select>
                             </div>
+                            <div className="col-lg-6 mb-5">
+                                <label htmlFor="" className="" onChange={handleInput}>Gender
+                                    <div className="d-flex">
+                                        <div className="form-check me-3">
+                                            <input
+                                                className="form-check-input"
+                                                type="radio"
+                                                name="gender"
+                                                id="gender"
+                                                value="male"
+                                            />
+                                            <label className="form-check-label" htmlFor="flexRadioDefault1">Male</label>
+                                        </div>
+                                        <div className="form-check">
+                                            <input
+                                                className="form-check-input"
+                                                type="radio"
+                                                name="gender"
+                                                id="gender"
+                                                defaultChecked=""
+                                                value="female"
+                                            />
+                                            <label className="form-check-label" htmlFor="flexRadioDefault2"> Female</label>
+                                        </div>
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button className="btn btn-primary" onClick={handleCreate}>Save changes</button>
                         </div>
                     </div>
                 </div>
-            </form>
-
+            </div>
         </div>
     )
 }
